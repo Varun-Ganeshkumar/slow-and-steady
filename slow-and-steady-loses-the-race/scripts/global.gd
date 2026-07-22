7acaa7d8aaca367b2e7c366c111fe675959b2246
Varun-Ganeshkumar <varunganeshkumar14@gmail.com>
2026-07-22 15:28:35 -0400

    Movement Changes
    Focused on making the movement feel better

---
+++ slow-and-steady-loses-the-race/scripts/global.gd
@@ -0,0 +1,3 @@
+extends Node
+
+# All variables that will need to be referenced in multiple scripts go here.
--- slow-and-steady-loses-the-race/scripts/player.gd
+++ slow-and-steady-loses-the-race/scripts/player.gd
@@ -1,24 +1,45 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+const SPEED = 280.0
+const JUMP_VELOCITY = -350.0
+var current_jumps = 0
+var how_long = 0
+@onready var coyote_timer: Timer = $CoyoteTimer
 
 func _physics_process(delta: float) -> void:
+	var was_on_floor = is_on_floor()
 	# Add the gravity.
-	if not is_on_floor():
+	if not is_on_floor() and coyote_timer.is_stopped():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	# Handle jump(s).
 
+	if Input.is_action_pressed("ui_accept"):
+		how_long += delta
+		if is_on_floor() or current_jumps < 2:
+			velocity.y = JUMP_VELOCITY
+			if how_long < .02:
+				current_jumps += 1
+			print(current_jumps)
+			print(how_long)
+			coyote_timer.stop()
+			how_long = 0
+			
+	if is_on_floor():
+		current_jumps = 0
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		if direction < 0:
+			$AnimatedSprite2D.flip_h = true
+		else:
+			$AnimatedSprite2D.flip_h = false
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	if was_on_floor and !is_on_floor():
+		coyote_timer.start()
