d49fc6ffcf367d2dac3d01c07a84567e7cfdcdeb
Varun-Ganeshkumar <varunganeshkumar14@gmail.com>
2026-07-23 11:41:36 -0400

    Movement Changes
    Made movement much smoother and had coyote time work with a double jump

--- slow-and-steady-loses-the-race/scripts/player.gd
+++ slow-and-steady-loses-the-race/scripts/player.gd
@@ -4,27 +4,24 @@ extends CharacterBody2D
 const SPEED = 280.0
 const JUMP_VELOCITY = -350.0
 var current_jumps = 0
-var how_long = 0
+const GRAVITY = 1000
 @onready var coyote_timer: Timer = $CoyoteTimer
+
 
 func _physics_process(delta: float) -> void:
 	var was_on_floor = is_on_floor()
 	# Add the gravity.
 	if not is_on_floor() and coyote_timer.is_stopped():
 		velocity += get_gravity() * delta
-
 	# Handle jump(s).
-
-	if Input.is_action_pressed("ui_accept"):
-		how_long += delta
-		if is_on_floor() or current_jumps < 2:
+	if Input.is_action_just_released("ui_accept") and current_jumps < 1 and !is_on_floor():
+		velocity.y = JUMP_VELOCITY / 4
+	
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor() or current_jumps < 1:
 			velocity.y = JUMP_VELOCITY
-			if how_long < .02:
-				current_jumps += 1
-			print(current_jumps)
-			print(how_long)
+			current_jumps += 1
 			coyote_timer.stop()
-			how_long = 0
 			
 	if is_on_floor():
 		current_jumps = 0
@@ -43,3 +40,5 @@ func _physics_process(delta: float) -> void:
 	
 	if was_on_floor and !is_on_floor():
 		coyote_timer.start()
+		if was_on_floor and !Input.is_action_just_pressed("ui_accept"):
+			current_jumps = -1
